376c8f421ceb97571a086b338653a0af9ccd6903
Poonam Kumari <poonamkumari@Poonams-MacBook-Pro.local>
2025-01-10 19:48:00 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { OrganiserEventComponent } from './components/organiser-event/organiser-event.component';
+import { VolunteerListComponent } from './components/volunteer-list/volunteer-list.component';
 
-const routes: Routes = [  { path: "login", component: LoginComponent, pathMatch: "full",data: {
-  title: 'Login'
-},},];
+const routes: Routes = [{
+  path: "login", component: LoginComponent, pathMatch: "full", data: {
+    title: 'Login'
+  },
+
+},
+{
+  path: "event", component: OrganiserEventComponent, pathMatch: "full", data: {
+  },
+
+},
+{
+  path: "volunteer-list", component: VolunteerListComponent, pathMatch: "full", data: {
+  },
+
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { OrganiserEventComponent } from './components/organiser-event/organiser-event.component';
+import { VolunteerListComponent } from './components/volunteer-list/volunteer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    OrganiserEventComponent,
+    VolunteerListComponent
   ],
   imports: [
     BrowserModule,
