24df7cd85a79bcc6af44f451d662e91e181d3a86
Poonam Kumari <poonamkumari@Poonams-MacBook-Pro.local>
2025-01-10 19:46:45 +0530

    basic components added

---
+++ src/app/components/organiser-event/organiser-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organiser-event',
+  templateUrl: './organiser-event.component.html',
+  styleUrls: ['./organiser-event.component.scss']
+})
+export class OrganiserEventComponent {
+
+}
